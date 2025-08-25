b754187c0f46702ec46fa35c6fc627277d1a9618
Cobe Liu <liucobe@gmail.com>
2025-08-25 18:30:59 -0400

    draft update

--- Sources/ExyteChat/Model/Draft.swift
+++ Sources/ExyteChat/Model/Draft.swift
@@ -12,18 +12,48 @@ public struct DraftMessage {
     public let recording: Recording?
     public let replyMessage: ReplyMessage?
     public let createdAt: Date
+    public let type: String?
+    public let requestStatus: String?
+    public let attachMent: Attachment?
+    public let receiverId: String?
+    public let isGroupInvite: Bool
+    public let attendanceGroupId: String?
+    public let reelData : ReelModel?
+    public let category : String?
+    public let linkedMessageId: String?
+    public let isFollowUpMessage: Bool
 
     public init(id: String? = nil, 
                 text: String,
                 medias: [Media],
                 recording: Recording?,
                 replyMessage: ReplyMessage?,
-                createdAt: Date) {
+                createdAt: Date,
+                type: String? = nil,
+                requestStatus:String? = nil,
+                attachMent: Attachment? = nil,
+                reelData : ReelModel? = nil,
+                receiverId: String? = nil,
+                isGroupInvite: Bool = false,
+                attendanceGroupId: String? = nil,
+                category: String? = nil,
+                linkedMessageId: String? = nil,
+                isFollowUpMessage: Bool = false) {
         self.id = id
         self.text = text
         self.medias = medias
         self.recording = recording
         self.replyMessage = replyMessage
         self.createdAt = createdAt
+        self.type = type
+        self.requestStatus = requestStatus
+        self.attachMent = attachMent
+        self.reelData = reelData
+        self.receiverId = receiverId
+        self.isGroupInvite = isGroupInvite
+        self.attendanceGroupId = attendanceGroupId
+        self.category = category
+        self.linkedMessageId = linkedMessageId
+        self.isFollowUpMessage = isFollowUpMessage
     }
 }
